8239c6995daefcff6fb2ac3be54132de1fb2f8c0
chaen-ing <96906242+chaen-ing@users.noreply.github.com>
2023-07-05 15:15:53 +0900

    2675번 : 문자열 반복

--- boj2675/Main.java
+++ boj2675/Main.java
@@ -2,11 +2,27 @@ package boj2675;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for(int k = 0; k < T; k++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+            String[] arr = str.split("");
 
-
+            for(int i = 0; i < arr.length; i++){
+                for(int j = 0; j < n; j++){
+                    sb.append(arr[i]);
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
     }
 }

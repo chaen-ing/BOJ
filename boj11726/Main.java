3284ff6828fa53a9c9214476d1e9a72bd3d9ef75
chaen-ing <kimce0405@naver.com>
2024-04-16 00:06:28 +0900

    11726 : 2*n 타일링

---
+++ boj11726/Main.java
@@ -0,0 +1,28 @@
+package boj11726;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int arr[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[1001];
+
+        Arrays.fill(arr,-1);
+        arr[1] = 1;
+        arr[2] = 2;
+
+        System.out.println(dp(n));
+    }
+    static int dp(int n){
+        if(arr[n] != -1)    return arr[n];
+        else{
+            arr[n] = (dp(n-1) + dp(n-2)) % 10007;
+        }
+        return arr[n];
+    }
+}

1032131204875dfe2daacb8d7dc4d68a293871b9
chaen-ing <kimce0405@naver.com>
2025-06-24 03:04:42 +0900

    11720 : 숫자의 합 구하기

--- boj11720/Main.java
+++ boj11720/Main.java
@@ -3,6 +3,11 @@ import java.io.*;
 
 public class Main {
     public static void main(String[]args) throws IOException{
+        //firstSolve();
+        secondSolve();
+    }
+
+    private static void firstSolve() throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
         br.readLine();
@@ -15,4 +20,22 @@ public class Main {
 
         System.out.println(sum);
     }
+
+    private static void secondSolve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+        char [] arr = str.toCharArray();
+
+        long sum = 0;
+        for(int i = 0; i < N; i++){
+            sum += arr[i] - '0';
+        }
+
+        System.out.println(sum);
+
+    }
+
 }

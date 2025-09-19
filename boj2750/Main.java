4ee04f6dfa9c837102282904f7f1d3329e368950
chaen-ing <kimce0405@naver.com>
2025-09-19 22:53:58 +0900

    2750 : 수 정렬하기 scd

--- boj2750/Main.java
+++ boj2750/Main.java
@@ -5,6 +5,39 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		// 초기화
+		int [] arr = new int [n];
+		for(int i = 0; i < n; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+
+		// 버블정렬
+		int temp;
+		for(int i = n - 1; i > 0; i--){
+			for(int j = 0; j < i; j++){
+
+				if(arr[j] > arr[j+1]){
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+
+			}
+		}
+
+		// 출력
+		for(int i = 0; i < n; i++){
+			System.out.println(arr[i]);
+		}
+
+	}
+
+	private static void first() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
@@ -22,6 +55,5 @@ public class Main {
 
 		bw.flush();
 		bw.close();
-
 	}
 }

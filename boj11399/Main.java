034372fd52de8c69d4accbcf60398333851b24b6
chaen-ing <kimce0405@naver.com>
2025-09-21 16:03:03 +0900

    11399 : ATM scd

--- boj11399/Main.java
+++ boj11399/Main.java
@@ -6,6 +6,39 @@ public class Main {
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 초기화
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] arr = new int [n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 삽입 정렬..인데 아닌듯
+        int temp;
+        for(int i = 1; i < n; i++){
+            for(int j = i; j > 0; j--){
+                if(arr[j] < arr[j-1]){
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+
+        // 합 구하기
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i] * (n - i);
+        }
+
+        System.out.println(sum);
+
+    }
+
+    private static void firstSolve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
         int[]arr = new int[N];
 

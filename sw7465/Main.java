a5bdd8c2188d8b5c932e308dd8a31f78c1858a19
chaen-ing <kimce0405@naver.com>
2026-03-09 14:53:40 +0900

    7465. 창용 마을 무리의 개수

---
+++ sw7465/Main.java
@@ -0,0 +1,66 @@
+package sw7465;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] parent;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			makeSet();
+
+			while (M-- > 0) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+
+				union(a, b);
+			}
+
+			HashSet<Integer> count = new HashSet<>();
+			for (int i = 1; i <= N; i++) {
+				count.add(find(parent[i]));
+			}
+
+			sb.append("#").append(tc).append(" ").append(count.size()).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	static void makeSet() {
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+	}
+
+	static int find(int x) {
+		if (parent[x] == x)
+			return x;
+		return parent[x] = find(parent[x]);    // 경로압축
+	}
+
+	static void union(int a, int b) {
+		int findA = find(a);
+		int findB = find(b);
+
+		if (findA == findB)
+			return;
+		parent[findB] = findA;
+	}
+}
